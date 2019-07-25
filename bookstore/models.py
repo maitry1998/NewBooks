c536e2db18baa61da17f084d92db49197b70be38
MkChauhan <maitrychauhan1998@gmail.com>
2019-07-25 19:54:50 +0530

    single book display displayed on the store

--- bookstore/models.py
+++ bookstore/models.py
@@ -21,3 +21,44 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE)
     publish_date=models.DateField(default=timezone.now)
     text=models.TextField()
+
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+    order_date = models.DateField(null=True)
+    payment_type = models.CharField(max_length=100, null=True)
+    payment_id = models.CharField(max_length=100, null=True)
+
+    def add_to_cart(self, book_id):
+        book = Book.objects.get(pk=book_id)
+        try:
+            preexisting_order = BookOrder.objects.get(book=book, cart=self)
+            preexisting_order.quantity += 1
+            preexisting_order.save()
+        except BookOrder.DoesNotExist:
+            new_order = BookOrder.objects.create(
+                book=book,
+                cart=self,
+                quantity=1
+            )
+            new_order.save()
+
+    def remove_from_cart(self, book_id):
+        book = Book.objects.get(pk=book_id)
+        try:
+            preexisting_order = BookOrder.objects.get(book=book, cart=self)
+            if preexisting_order.quantity > 1:
+                preexisting_order.quantity -= 1
+                preexisting_order.save()
+            else:
+                preexisting_order.delete()
+        except BookOrder.DoesNotExist:
+            pass
+
+
+class BookOrder(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
